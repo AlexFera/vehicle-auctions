2d4f1b822c69ce107316b5739c179050bdba454e
Alex Fera <alexandru.fera@arnia.ro>
2018-08-20 19:04:52 +0300

    map saleid on the Bid entity

--- src/Database/dbo/Stored Procedures/Bid_ListByLotId.sql
+++ src/Database/dbo/Stored Procedures/Bid_ListByLotId.sql
@@ -3,13 +3,16 @@ AS
 SELECT
 	-- Bid
 	b.Id
+	,b.LotId
 	,b.Amount
 	,b.PlacedDate
+	,l.SaleId
 	-- Buyer
 	,bu.Id
 	,anu.UserName
 FROM [dbo].[Bid] b WITH (NOLOCK)
 INNER JOIN [dbo].[Buyer] bu WITH (NOLOCK) ON b.BuyerId = bu.Id
 INNER JOIN [dbo].[AspNetUsers] anu WITH (NOLOCK) ON bu.UserId = anu.Id
+INNER JOIN [dbo].[Lot] l WITH (NOLOCK) ON b.LotId = l.Id
 ORDER BY b.Id
 
